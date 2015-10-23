58cc8f06ef9647570cdef579967ceeeeb7a302c1
Oliver Forral <intrepion@gmail.com>
2015-10-23 00:11:17 -0700

    Video 2.2 - Adding Dynamic Content

---
+++ src/components/page_header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PageHeader extends React.Component {
+    render() {
+        return (
+            <div className='page-header'>
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export default PageHeader;
