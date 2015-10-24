8969ad999a2cd8220b6f69cfdc7a984d7dc5715f
Oliver Forral <intrepion@gmail.com>
2015-10-24 00:24:44 -0700

    Video 4.3 - State Management

--- src/stores/orders_store.js
+++ src/stores/orders_store.js
@@ -1,9 +1,11 @@
 import alt from '../alt';
+import ImmutableUtil from 'alt/utils/ImmutableUtil';
+import Immutable from 'immutable';
 import OrdersActions from '../actions/orders_actions';
 
 class OrdersStore {
     constructor() {
-        this.orders = [];
+        this.orders = Immutable.List();
         this.selectedStatus = 'all';
         this.amountFilter = null;
 
@@ -32,4 +34,4 @@ class OrdersStore {
     }
 }
 
-export default alt.createStore(OrdersStore);
+export default alt.createStore(ImmutableUtil(OrdersStore));
